6006a7927251f9d1a37ee9ec283b040d4bec292c
Martzyy <Martzyy@users.noreply.github.com>
2020-04-08 01:04:28 +0800

    update 2

--- NetworkScience2/PythonApplication1/main.py
+++ NetworkScience2/PythonApplication1/main.py
@@ -8,10 +8,10 @@ class node:
     relations:dict #relations in form of <connectednode object>:<types of relations in a list>
 
     #init function
-    def __init__(self,name,attributes,connectednodes):
+    def __init__(self,name,attributes,relations):
         self.name = name
         self.attributes = attributes
-        self.connected = connected
+        self.relations = relations
         return
 
     def __init__(self):
@@ -23,6 +23,7 @@ class node:
 class kg:
     network:list
 
+    #init methods
     def __init__(self,network):
         self.network = network
         return
@@ -30,13 +31,37 @@ class kg:
     def __init__(self):
         self.network = []
 
-    def parsedata(self,datapath):
+    #critical methods
+    def generateInversePath(self):
+        for nod in self.network:
+            for item in nod.relations:
+                for i,tupl in enumerate(nod.relations[item]):
+                    print(nod.name,item,i,tupl[0])
+                    try:
+                        self.search(item).relations[nod.name]
+                    except Exception:
+                        self.search(item).relations[nod.name] = []
+                    self.search(item).relations[nod.name].append((tupl[0],1))
+        return
+    def search(self,name):
+        for item in self.network:
+            if item.name == name:
+                return item
+    
+    def show(self):
+        for item in self.network:
+            print('name : '+item.name)
+            print('attributes : '+str(item.attributes))
+            print('relations : '+str(item.relations))
+            print('')
+        return
+
+    def parseData(self,datapath):
         xmlp = etree.XMLParser(recover = True)
         itertree = etree.iterparse(datapath, load_dtd = True, events=("start", "end"))
         itertree = iter(itertree)
         previous = None #same as proj1
         for event, elem in itertree:
-            print(event,elem.tag,elem.text)
             if event == 'start':
                 if elem.tag == 'node':
                     previous = node()
@@ -46,15 +71,20 @@ class kg:
                     previous.attributes = elem.text
                 elif elem.tag == 'relation':
                     lst = elem.text.split(":",1)
-                    previous.relations[lst[0]] = (lst[1],0) #(relation,1) is inverse of (relation,0)
+                    try: 
+                        previous.relations[lst[0]]
+                    except Exception:
+                        previous.relations[lst[0]] = []
+                    previous.relations[lst[0]].append((lst[1],0)) #(relation,1) is inverse of (relation,0)
             elif event == 'end' and elem.tag == 'node':
                 self.network.append(previous)
                 previous = None
             elem.clear()
         return
 
+
 datapath = 'database.xml'
 kgraph = kg()
-kgraph.parsedata(datapath)
-for item in kgraph.network:
-    print(item.name)
+kgraph.parseData(datapath)
+kgraph.generateInversePath()
+kgraph.show()
